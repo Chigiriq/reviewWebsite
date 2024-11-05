541d3754c7e80a8caef0e8552890b37988f4dfbc
Alex S <faeyntt@gmail.com>
2024-11-05 12:21:44 -0700

    templates

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("", TemplateView.as_view(template_name="home.html"), name="home"), # new
+    
+    path("", include('reviews.urls')),
+    # path('products/', include('products.urls')),
+    # path('users/', include('users.urls')),
+] + debug_toolbar_urls()
