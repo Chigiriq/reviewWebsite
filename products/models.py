c85687ae565e8216f0e50c83c090a7e128896bce
Alex S <faeyntt@gmail.com>
2024-11-19 13:56:47 -0700

    product detail/global back button

--- products/models.py
+++ products/models.py
@@ -7,9 +7,12 @@ class Product(models.Model):
     # image = ???
     price = models.PositiveIntegerField(null=True, blank=True)
 
+    description = models.CharField(max_length=30, blank=True, null=True)
+    
+
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
-        return reverse("model_detail", kwargs={"pk": self.pk})
+        return reverse("product_detail", kwargs={"pk": self.pk})
     
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView, FormView
 from django.urls import reverse
@@ -9,3 +9,11 @@ from .models import Product
 class ProductListView(ListView):
     model = Product
     template_name = "product_list.html"
+
+class ProductDetailView(DetailView):
+    # model = Product
+    template_name = "product_detail.html"
+
+    def get(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        return render(request, self.template_name, {"product": product})
