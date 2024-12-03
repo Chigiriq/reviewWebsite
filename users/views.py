d9d95d57506cd3b6a125d27f9fd18a3550e958ab
Jules Stier <juliostier1128@gmail.com>
2024-12-03 12:05:35 -0700

    added to profile page

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView, TemplateView
+from django.views.generic import CreateView, DetailView, TemplateView, UpdateView
 from django.urls import reverse_lazy
 from .models import CustomUser
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -15,4 +15,9 @@ class SignUpView(CreateView):
 class ProfileDetail(LoginRequiredMixin, TemplateView):
     model = CustomUser
     template_name = "profile.html"
+
+class PasswordChangeView(UpdateView):
+    model = CustomUser
+    template_name = "password_change_view.html"
+    fields = ["Old Password", "New Password", "Confirm New Password"]
 
