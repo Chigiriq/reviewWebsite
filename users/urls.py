d71b5131ec344b4d766aaa8545d4054a69f3a302
Alex S <faeyntt@gmail.com>
2024-12-03 12:15:09 -0700

    authentication links working again

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from .views import SignUpView, ProfileDetail
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    # path("login/", LoginView.as_view(), name="login"),
     #path("<int:pk>/", ProfileDetail.as_view(), name = "profile")
     path('profile/', ProfileDetail.as_view(), name="profile"),
 ]
